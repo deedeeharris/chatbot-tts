a3feecd60968e52761cad98a210aec9b21c97b1d
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2024-04-25 15:12:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import os
-import openai
-import random
+import utils
 import streamlit as st
-from datetime import datetime
 from streaming import StreamHandler
+
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
@@ -13,99 +11,33 @@ st.header('Context aware chatbot')
 st.write('Enhancing Chatbot Interactions through Context Awareness')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/2_%E2%AD%90_context_aware_chatbot.py)')
 
-# Decorator
-def enable_chat_history(func):
-    if os.environ.get("OPENAI_API_KEY"):
+class ContextChatbot:
 
-        # To clear chat history after switching chatbot
-        current_page = func.__qualname__
-        if "current_page" not in st.session_state:
-            st.session_state["current_page"] = current_page
-        if st.session_state["current_page"] != current_page:
-            try:
-                st.cache_resource.clear()
-                del st.session_state["current_page"]
-                del st.session_state["messages"]
-            except:
-                pass
-
-        # To show chat history on UI
-        if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-        for msg in st.session_state["messages"]:
-            st.chat_message(msg["role"]).write(msg["content"])
-
-    def execute(*args, **kwargs):
-        func(*args, **kwargs)
-    return execute
-
-def display_msg(msg, author):
-    """Method to display message on the UI
-
-    Args:
-        msg (str): message to display
-        author (str): author of the message -user/assistant
-    """
-    st.session_state.messages.append({"role": author, "content": msg})
-    st.chat_message(author).write(msg)
-
-def configure_openai():
-    openai_api_key = st.sidebar.text_input(
-        label="OpenAI API Key",
-        type="password",
-        value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',
-        placeholder="sk-..."
-        )
-    if openai_api_key:
-        st.session_state['OPENAI_API_KEY'] = openai_api_key
-        os.environ['OPENAI_API_KEY'] = openai_api_key
-    else:
-        st.error("Please add your OpenAI API key to continue.")
-        st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
-        st.stop()
-
-    model = "gpt-3.5-turbo"
-    try:
-        client = openai.OpenAI()
-        available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
-        available_models = sorted(available_models, key=lambda x: x["created"])
-        available_models = [i["id"] for i in available_models]
-
-        model = st.sidebar.selectbox(
-            label="Model",
-            options=available_models,
-            index=available_models.index(st.session_state['OPENAI_MODEL']) if 'OPENAI_MODEL' in st.session_state else 0
-        )
-        st.session_state['OPENAI_MODEL'] = model
-    except openai.AuthenticationError as e:
-        st.error(e.body["message"])
-        st.stop()
-    except Exception as e:
-        print(e)
-        st.error("Something went wrong. Please try again later.")
-        st.stop()
-    return model
-
-@st.cache_resource
-def setup_chain():
-    memory = ConversationBufferMemory()
-    llm = ChatOpenAI(model_name=configure_openai(), temperature=0, streaming=True)
-    chain = ConversationChain(llm=llm, memory=memory, verbose=True)
-    return chain
-
-def main():
-    chain = setup_chain()
-    user_query = st.chat_input(placeholder="Ask me anything!")
-    if user_query:
-        display_msg(user_query, 'user')
-        with st.chat_message("assistant"):
-            st_cb = StreamHandler(st.empty())
-            result = chain.invoke(
-                {"input":user_query},
-                {"callbacks": [st_cb]}
-            )
-            response = result["response"]
-            st.session_state.messages.append({"role": "assistant", "content": response})
+    def __init__(self):
+        self.openai_model = utils.configure_openai()
+    
+    @st.cache_resource
+    def setup_chain(_self):
+        memory = ConversationBufferMemory()
+        llm = ChatOpenAI(model_name=_self.openai_model, temperature=0, streaming=True)
+        chain = ConversationChain(llm=llm, memory=memory, verbose=True)
+        return chain
+    
+    @utils.enable_chat_history
+    def main(self):
+        chain = self.setup_chain()
+        user_query = st.chat_input(placeholder="Ask me anything!")
+        if user_query:
+            utils.display_msg(user_query, 'user')
+            with st.chat_message("assistant"):
+                st_cb = StreamHandler(st.empty())
+                result = chain.invoke(
+                    {"input":user_query},
+                    {"callbacks": [st_cb]}
+                )
+                response = result["response"]
+                st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
-    main()
+    obj = ContextChatbot()
+    obj.main()
