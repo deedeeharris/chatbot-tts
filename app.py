28bd75ec47ba29b68bfe035f45d120972e4bffc9
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2024-04-25 14:35:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ with col1:
 
 with col2:
     st.title("Avatar")
-    st.video(sacks_video_bytes, start_time=0, loop_start=0, loop_end=0, format="mp4", width=300, height=300)
+    st.video(sacks_video_bytes, start_time=0,format="mp4", width=300, height=300)
